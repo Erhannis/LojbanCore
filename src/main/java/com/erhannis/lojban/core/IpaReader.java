6c9caddca6e2b3c0d2f244af3acfb75d73a6cdc5
erhannis <Eyeillus@gmail.com>
2017-06-14 08:48:09 -0400

    Trying to get the reader to work

--- src/main/java/com/erhannis/lojban/core/IpaReader.java
+++ src/main/java/com/erhannis/lojban/core/IpaReader.java
@@ -5,10 +5,29 @@
  */
 package com.erhannis.lojban.core;
 
+import java.io.FileNotFoundException;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 /**
  *
  * @author erhannis
  */
 public class IpaReader {
-  
+  public static void test() throws ScriptException, NoSuchMethodException, FileNotFoundException {
+    ScriptEngineManager manager = new ScriptEngineManager();
+    ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+    engine.eval(new java.io.FileReader("mespeak.full.js"));
+    engine.eval("meSpeak.loadConfig(\"mespeak_config.json\");");
+    engine.eval("meSpeak.loadVoice(\"en.json\");");
+    
+    Object meSpeak = engine.get("meSpeak");
+
+    Invocable inv = (Invocable) engine;
+    //inv.invokeFunction("hello", "Scripting!!" );
+    engine.eval("meSpeak.play(meSpeak.speak('[[DIs Iz @n Ig'zA:mpl 'sEnt@ns]]', { 'rawdata': 'mime' }));");
+  }
 }
--- src/main/java/com/erhannis/lojban/core/Main.java
+++ src/main/java/com/erhannis/lojban/core/Main.java
@@ -10,17 +10,21 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import javax.script.ScriptException;
 
 /**
  *
  * @author erhannis
  */
 public class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ScriptException, NoSuchMethodException {
+    IpaReader.test();
+    /*
     String text = new String(Files.readAllBytes(FileSystems.getDefault().getPath("alice.txt")));
     String[] a = LojbanToIpa.lojbanToIpa(text);
     for (int i = 0; i < a.length; i++) {
       System.out.println(i + " - " + a[i]);
     }
+    */
   }
 }
